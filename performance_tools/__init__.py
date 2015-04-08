dc0480a1f9fe10a192f370edd1a0d540760fb528
PeRDy <perdy.hh@gmail.com>
2015-04-08 18:42:53 +0200

    Init values

--- performance_tools/__init__.py
+++ performance_tools/__init__.py
@@ -1,1 +1,12 @@
-__author__ = 'perdy'
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Performance tools"""
+__version__ = '0.1.0'
+__license__ = 'GPLv3'
+
+__author__ = 'José Antonio Perdiguero López'
+__email__ = 'perdy.hh@gmail.com'
+
+__url__ = 'https://github.com/PeRDy/performance-tools'
+__description__ = 'Python package that provides some specific tools for performance analysis and improvements.'
